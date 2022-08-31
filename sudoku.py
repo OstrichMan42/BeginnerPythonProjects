3b1b713c9027ffc9f831bb1ef0a64dbdd0da72a6
Caetano <49847688+OstrichMan42@users.noreply.github.com>
2022-08-31 14:44:57 -0700

    started solve_sudoku, find_next_empty, & is_valid

--- sudoku.py
+++ sudoku.py
@@ -2,15 +2,45 @@
 # OPTIONAL HELPER FUNCTIONS
 ############################################################
 
+# puzzle.board = [][] (9x9 by default)
+
+from random import randint
+
+
 def find_next_empty(puzzle):
     # finds the next row, col on puzzle that's not filled yet --> we represent these with -1
     # returns a row, col tuple (or (None, None) if there is none)
-    pass
+    for row in range(9):
+        for col in range(9):
+            if puzzle[row][col] == -1: return True
+
+    return (None, None)
 
 def is_valid(puzzle, guess, row, col):
     # figures out whether the guess at the row/col of the puzzle is a valid guess
     # returns True or False
-    pass
+
+    # sudoku rules
+    # check own box
+    # iterate over neighboring x positions
+    for r in range(max(0, row-1), min(row+1, 9-1)):
+        # iterate over neighboring y positions
+        for c in range(max(0, col-1), min(col+1, 9-1)):
+            # skip my own box
+            if row == row and col == col: continue
+
+            # now check the inner column
+            if guess in puzzle[row][col]:
+                pass
+
+    # check row and column
+    for r in puzzle[row]:
+        if guess in r: return False
+    for c in [puzzle[i-1][col] for i in range(3)]:
+        if guess in c: return False
+
+
+    return True
 
 ############################################################
 # SOLVER IMPLEMENTATION
@@ -22,11 +52,32 @@ def solve_sudoku(puzzle):
     # return solution
 
     # step 1: choose somewhere on the puzzle to make a guess
+    row, col = find_next_empty(puzzle)
+
     # step 1.1: if there's nowhere left, then we're done because we only allowed valid inputs
+    if row is None:
+        return True
+
     # step 2: if there is a place to put a number, then make a guess between 1 and 9
-    # step 3: check if this is a valid guess
-    # step 3.1: if this is a valid guess, then place it at that spot on the puzzle
-    # step 4: then we recursively call our solver!
-    # step 5: it not valid or if nothing gets returned true, then we need to backtrack and try a new number
+    guess = randint(1, 9)
+    guessed = set()
+    while len(guessed) < 9:
+        # step 3: check if this is a valid guess
+        if is_valid(puzzle, guess, row, col):
+
+            # step 3.1: if this is a valid guess, then place it at that spot on the puzzle
+            puzzle[row][col] = guess
+
+            # step 4: then we recursively call our solver!
+            if solve_sudoku(puzzle):
+                return True
+
+        # step 5: it not valid or if nothing gets returned true, then we need to backtrack and try a new number
+        puzzle[row][col] = -1
+        guessed.add(guess)
+        guess = 1
+        while guess in guessed:
+            guess += 1
+
     # step 6: if none of the numbers that we try work, then this puzzle is UNSOLVABLE!!
-    pass
+    return False
