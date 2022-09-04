8f0e9d68e1c6cb7e987c53cf67277c85d1671fbf
Caetano Hyams <cjhyams@ucsc.edu>
2022-09-04 11:45:55 -0700

    Completed Graph.py

--- graph-composer/graph.py
+++ graph-composer/graph.py
@@ -15,41 +15,55 @@ import random
 
 class Vertex(object):
     def __init__(self, value):
-        pass
+        self.value = value
+        self.adjacent = {} # dictionary of edges to wieght
+        self.neighbors = []
+        self.neighborWeights = []
+        
 
     def add_edge_to(self, vertex, weight=0):
-        pass
+        self.adjacent[vertex] = weight
 
     def increment_edge(self, vertex):
-        pass
+        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1
 
     def get_adjacent_nodes(self):
-        pass
+        return self.adjacent
 
     # initializes probability map
     def get_probability_map(self):
-        pass
+        for (vertex, weight) in self.adjacent.items():
+                self.neighbors.append(vertex)
+                self.neighborWeights.append(weight)
 
     def next_word(self):
-        pass
+        # pick random adjacent neighbor, favoring weighted edges
+        return random.choices(self.neighbors, self.neighborWeights)
+        
 
 
-
+# put verticies in a graph
 class Graph(object):
     def __init__(self):
-        pass
+        # map of string to vertex
+        self.verticies = {}
 
     def get_vertex_values(self):
-        pass
+        # values of all verticies
+        return set(self.verticies.keys)
 
     def add_vertex(self, value):
-        pass
+        self.verticies[value] = Vertex(value)
 
     def get_vertex(self, value):
-        pass
+        # create node if none exists
+        if value not in self.verticies:
+            self.add_vertex(value)
+        return self.verticies[value]
 
     def get_next_word(self, current_vertex):
-        pass
+        self.verticies[current_vertex.value].next_word()
 
     def generate_probability_mappings(self):
-        pass
+        for vertex in self.verticies.values:
+            vertex.get_probability_map()
